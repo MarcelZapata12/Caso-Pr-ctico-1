aab64a68e72c43e0f6fe1e0c704265e9508a74b5
Marcel1203 <Mazaca120703@gmail.com>
2025-03-04 20:16:41 -0600

    FrontEnd Done

---
+++ Caso Practico/FrontEnd/Helpers/Implementations/ProgramaHelper.cs
@@ -0,0 +1,100 @@
+﻿using FrontEnd.ApiModels;
+using FrontEnd.Helpers.Interfaces;
+using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers.Implementations
+{
+    public class ProgramaHelper : IProgramaHelper
+    {
+        IServiceRepository _ServiceRepository;
+
+        ProgramaViewModel Convertir(ProgramaAPI programa)
+        {
+            ProgramaViewModel programaViewModel = new ProgramaViewModel
+            {
+                ProgramaId = programa.ProgramaId,
+                Nombre = programa.Nombre,
+                Tipo = programa.Tipo,
+                Categoria = programa.Categoria
+            };
+            return programaViewModel;
+        }
+
+
+        public ProgramaHelper(IServiceRepository serviceRepository)
+        {
+            _ServiceRepository = serviceRepository;
+        }
+
+        public ProgramaViewModel Add(ProgramaViewModel programa)
+        {
+            HttpResponseMessage response = _ServiceRepository.PostResponse("api/Programa", programa);
+            if (response.IsSuccessStatusCode)
+            {
+
+                var content = response.Content.ReadAsStringAsync().Result;
+            }
+            return programa;
+        }
+
+        public void Delete(int id)
+        {
+            HttpResponseMessage response = _ServiceRepository.DeleteResponse("api/Programa/" + id.ToString());
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Error al eliminar el programa");
+            }
+        }
+
+
+        public List<ProgramaViewModel> GetProgramas()
+        {
+            HttpResponseMessage responseMessage = _ServiceRepository.GetResponse("api/Programa");
+            List<ProgramaAPI> programas = new List<ProgramaAPI>();
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                programas = JsonConvert.DeserializeObject<List<ProgramaAPI>>(content);
+            }
+            List<ProgramaViewModel> lista = new List<ProgramaViewModel>();
+            foreach (var programa in programas)
+            {
+                lista.Add(Convertir(programa));
+            }
+            return lista;
+        }
+
+        public ProgramaViewModel GetPrograma(int? id)
+        {
+            HttpResponseMessage responseMessage = _ServiceRepository.GetResponse("api/Programa/" + id.ToString());
+            ProgramaAPI programa = new ProgramaAPI();
+            if (responseMessage != null)
+            {
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                programa = JsonConvert.DeserializeObject<ProgramaAPI>(content);
+            }
+
+            ProgramaViewModel resultado = Convertir(programa);
+
+
+            return resultado;
+        }
+
+        public ProgramaViewModel Update(ProgramaViewModel programa)
+        {
+            HttpResponseMessage response = _ServiceRepository.PutResponse("api/Programa", programa);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<ProgramaViewModel>(content);
+            }
+            else
+            {
+                throw new Exception("Error al actualizar el programa");
+            }
+        }
+
+    }
+}
+
---
+++ Caso Practico/FrontEnd/Models/ProgramaViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FrontEnd.Models
+{
+    public class ProgramaViewModel
+    {
+        public int ProgramaId { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public int? Tipo { get; set; }
+
+        public int? Categoria { get; set; }
+    }
+}
